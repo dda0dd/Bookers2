cce2843fa8666465d85d422a0c8a0ceab5e5b10a
dda0dd <168615435+dda0dd@users.noreply.github.com>
2024-05-21 12:54:05 +0000

    [Update] 4-課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
 # 画像投稿の画面を表示するアクションメソッド
+
+# 各アクション実行前に実行したい処理指定
+before_action :is_matching_login_user, only: [:edit]
   def new
 # form_withに渡すための「空のモデル」を用意
     @book = Book.new
@@ -18,6 +21,8 @@ class BooksController < ApplicationController
 # バリデーションで保存できなかった時はsaveメソッドがfalseになり、renderでbooks/new.html.erbが表示され投稿ページを再表示する設定
     else
       @books = Book.all
+# @userを定義
+      @user = current_user
       render :index
     end
   end
@@ -29,35 +34,46 @@ class BooksController < ApplicationController
     # @books = Book.page(params[:page])
 # modelを全て選択
     @books = Book.all
+# @userを定義
+    @user = current_user
+# @bookを定義
+    @book = Book.new
   end
 # 詳細画面が表示されるように設定
   def show
     @book = Book.find(params[:id])
+# @userを定義
+    @user = @book.user
+    @book_new = Book.new
   end
 
 # 投稿を削除するdestroyアクションの処理(投稿の削除後は一覧に遷移)記述
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to Books_path
+    redirect_to books_path
   end
 
   def edit
+    @book = Book.find(params[:id])
+    # unless @book.id == current_user.id
+    #   redirect_to books_path
+    # end
   end
 
   def update
 # フォームに入力されたデータ(body,title,image)が@bookに格納される
-    @book = Book.new(book_params)
+    @book = Book.find(params[:id])
  # @book(投稿データ)のuser_idを、current_user.id(今ログインしているユーザーのID)に指定することで投稿データに、今ログイン中のユーザーのIDを持たせる
-    @book.user_id = current_user.id
-    if @book.save
+    if @book.update(book_params)
 # フラッシュメッセージ(books/showへリンク)
       flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
 # バリデーションで保存できなかった時はsaveメソッドがfalseになり、renderでbooks/new.html.erbが表示され投稿ページを再表示する設定
     else
-      @books = Book.all
-      
+      render :edit
+
+    end
   end
 
 
@@ -67,4 +83,13 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :image, :body)
   end
-end
+  
+# アクセス制限
+  def is_matching_login_user
+    @book = Book.find(params[:id])
+    unless @book.id == current_user.id
+      redirect_to books_path
+      
+    end
+  end
+  end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 # ユーザの詳細ページ
+
+# 各アクション実行前に実行したい処理指定
+before_action :is_matching_login_user, only: [:edit]
 # URLに記載されたIDを参考に、必要なUserモデルを取得する処理
   def show
 # エラー時にroutesの順番が関係しているかも
@@ -13,15 +16,21 @@ class UsersController < ApplicationController
 # 編集機能用のアクションを定義
   def edit
     @user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to users_path
+    end
   end
 # 更新機能を作成
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
+    if @user.update(user_params)
 # サクセスメッセージ（successfullyの言葉が含む）
   # users/showへリンク
-    flash[:success] = "You have updated user successfully."
-    redirect_to user_path(user.id)
+    flash[:notice] = "You have updated user successfully."
+    redirect_to user_path(@user.id)
+  else
+    render :edit
+  end
   end
 # ユーザ一覧作成
   def index
@@ -29,11 +38,20 @@ class UsersController < ApplicationController
     @user = current_user
 # 上書きされるので@userを複数形にする
     @users = User.all
+    @book = Book.new
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+# アクセス制限
+  def is_matching_login_user
+    @user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to users_path
+    end
   end
 end
